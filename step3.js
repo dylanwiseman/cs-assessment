0da8de7a8c22f2ba3e223aa603bddc6fbc495d19
Dylan Wiseman <dylan.wiseman22@gmail.com>
2021-07-23 12:13:33 -0600

    my edit

--- step3.js
+++ step3.js
@@ -32,7 +32,7 @@ const sumZero = (array) => {
     let count = 0
     for (let i = 0; i < array.length; i++) {
         for (let j = i + 1; j < array.length; j++) {
-            if (i + j === 0) {
+            if (arr[i] + arr[j] === 0) {
                 count++
             }
         }
